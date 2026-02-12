d51e23a766a560642d80ed2d35384f676b435e34
ermalkrz <ermalkrz@gmail.com>
2026-02-12 14:41:49 +0100

    Add operate function to handle calculator operations

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 };
+
+// Operate function that calls the appropriate math function
+const operate = function(operator, a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+};
